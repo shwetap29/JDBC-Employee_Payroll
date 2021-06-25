90b826e65eb1a33c3574e4a533c1904ef1027e7c
shwetap29 <shwetaprasad48@gmail.com>
2021-06-25 08:10:22 +0530

    Setting Up connection with Mysql DB

---
+++ src/main/java/com/bridgelabz/DBDemo.java
@@ -0,0 +1,29 @@
+package com.bridgelabz;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+public class DBDemo {
+    public static void main(String[] args) {
+        String jdbcURL ="jdbc:mysql://localhost:3386/payroll_service?useSSL=false";
+        String username = "root";
+        String password = "ASDF@123QW";
+        Connection connection;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver loaded!");
+        } catch (ClassNotFoundException e){
+            throw new IllegalStateException("Cannot find the driver in the classpath!", e);
+        }
+        listDrivers();
+    }
+    private static void listDrivers() {
+        Enumeration<Driver> driverList = DriverManager.getDrivers();
+        while (driverList.hasMoreElements()) {
+            Driver driverClass = (Driver) driverList.nextElement();
+            System.out.println(" "+driverClass.getClass().getName());
+        }
+    }
+}
